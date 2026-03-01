7e63e407923e56df7562fd2c431a86a30df2d517
ldyasada-Amb <ld.yasacamaa@uea.edu.ec>
2026-03-01 09:33:16 -0500

    jj

---
+++ semana11/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program // Traductor de español a inglés
+{
+    static Dictionary<string, string> diccionario = new Dictionary<string, string>()
+    {
+        {"tiempo", "time"},
+        {"persona", "person"},
+        {"año", "year"},
+        {"camino", "way"},
+        {"día", "day"},
+        {"cosa", "thing"},
+        {"hombre", "man"},
+        {"mundo", "world"},
+        {"vida", "life"},
+        {"mano", "hand"},
+        {"parte", "part"},
+        {"niño", "child"},
+        {"ojo", "eye"},
+        {"mujer", "woman"},
+        {"lugar", "place"},
+        {"trabajo", "work"},
+        {"semana", "week"},
+        {"caso", "case"},
+        {"punto", "point"},
+        {"gobierno", "government"},
+        {"empresa", "company"}
+    };
+
+    static void Main() //menu para traducir frases o agregar palabras al diccionario
+    {
+        string opcion;
+        do
+        {
+            Console.WriteLine("\n==================== MENÚ ====================");
+            Console.WriteLine("1. Traducir una frase");
+            Console.WriteLine("2. Agregar palabras al diccionario");
+            Console.WriteLine("0. Salir");
+            Console.Write("Seleccione una opción: ");
+            opcion = Console.ReadLine();
+
+            if (opcion == "1")
+            {
+                TraducirFrase();
+            }
+            else if (opcion == "2")
+            {
+                AgregarPalabra();
+            }
+            else if (opcion == "0")
+            {
+                Console.WriteLine("Saliendo...");
+            }
+            else
+            {
+                Console.WriteLine("Opción inválida.");
+            }
+
+        } while (opcion != "0");
+    }
+
+    static void TraducirFrase() // Traduce una frase ingresada por el usuario utilizando el diccionario
+    {
+        Console.Write("Ingrese la frase: ");
+        string frase = Console.ReadLine();
+        string[] palabras = frase.Split(' ');
+
+        for (int i = 0; i < palabras.Length; i++)
+        {
+            string limpia = palabras[i].Trim(",.;:!?".ToCharArray()).ToLower();
+            if (diccionario.ContainsKey(limpia))
+            {
+                palabras[i] = palabras[i].Replace(limpia, diccionario[limpia]);
+            }
+        }
+
+        Console.WriteLine("Traducción: " + string.Join(" ", palabras));
+    }
+
+    static void AgregarPalabra() // Permite al usuario agregar una nueva palabra al diccionario con su traducción correspondiente|
+    {
+        Console.Write("Palabra en español: ");
+        string esp = Console.ReadLine().ToLower();
+
+        Console.Write("Traducción en inglés: ");
+        string eng = Console.ReadLine().ToLower();
+
+        if (!diccionario.ContainsKey(esp))
+        {
+            diccionario.Add(esp, eng);
+            Console.WriteLine("Agregada correctamente.");
+        }
+        else
+        {
+            Console.WriteLine("La palabra ya existe.");
+        }
+    }
+}
+
--- semana11/obj/Debug/net10.0/semana11.AssemblyInfo.cs
+++ semana11/obj/Debug/net10.0/semana11.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("semana3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("semana11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbb603b4158bf88027b3eb4fc0e084892c37fe34")]
-[assembly: System.Reflection.AssemblyProductAttribute("semana3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("semana3")]
+[assembly: System.Reflection.AssemblyProductAttribute("semana11")]
+[assembly: System.Reflection.AssemblyTitleAttribute("semana11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
