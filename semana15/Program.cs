e165b2661588733f211cee3d5f6f2c9d0e4817aa
ldyasada-Amb <ld.yasacamaa@uea.edu.ec>
2026-04-01 09:33:21 -0500

    traajo final

---
+++ semana15/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace VuelosArbol
+{
+    public class Vuelo
+    {
+        public string Origen { get; set; }
+        public string Destino { get; set; }
+        public double Precio { get; set; }
+
+        public Vuelo(string origen, string destino, double precio)
+        {
+            Origen = origen;
+            Destino = destino;
+            Precio = precio;
+        }
+
+        public override string ToString()
+        {
+            return $"{Origen} -> {Destino} | Precio: ${Precio}";
+        }
+    }
+
+    public class NodoVuelo
+    {
+        public Vuelo Vuelo { get; set; }
+        public NodoVuelo Izquierdo { get; set; }
+        public NodoVuelo Derecho { get; set; }
+
+        public NodoVuelo(Vuelo vuelo)
+        {
+            Vuelo = vuelo;
+            Izquierdo = null;
+            Derecho = null;
+        }
+    }
+
+    public class ArbolVuelos
+    {
+        public NodoVuelo Raiz { get; set; }
+
+        public void Insertar(Vuelo vuelo)
+        {
+            Raiz = InsertarRecursivo(Raiz, vuelo);
+        }
+
+        private NodoVuelo InsertarRecursivo(NodoVuelo nodo, Vuelo vuelo)
+        {
+            if (nodo == null)
+                return new NodoVuelo(vuelo);
+
+            if (vuelo.Precio < nodo.Vuelo.Precio)
+                nodo.Izquierdo = InsertarRecursivo(nodo.Izquierdo, vuelo);
+            else
+                nodo.Derecho = InsertarRecursivo(nodo.Derecho, vuelo);
+
+            return nodo;
+        }
+
+        public void MostrarOrdenado(NodoVuelo nodo)
+        {
+            if (nodo != null)
+            {
+                MostrarOrdenado(nodo.Izquierdo);
+                Console.WriteLine(nodo.Vuelo);
+                MostrarOrdenado(nodo.Derecho);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArbolVuelos arbol = new ArbolVuelos();
+
+            arbol.Insertar(new Vuelo("Quito", "Guayaquil", 80));
+            arbol.Insertar(new Vuelo("Quito", "Cuenca", 60));
+            arbol.Insertar(new Vuelo("Guayaquil", "Madrid", 750));
+            arbol.Insertar(new Vuelo("Quito", "Bogotá", 200));
+            arbol.Insertar(new Vuelo("Cuenca", "Quito", 55));
+
+            Console.WriteLine("Vuelos ordenados por precio:");
+            arbol.MostrarOrdenado(arbol.Raiz);
+
+            Console.ReadLine();
+        }
+    }
+}
--- semana15/obj/Debug/net10.0/semana15.AssemblyInfo.cs
+++ semana15/obj/Debug/net10.0/semana15.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("semana3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("semana15")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07c9d5d2c011f84c49a864977f6748df82ad0c5")]
-[assembly: System.Reflection.AssemblyProductAttribute("semana3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("semana3")]
+[assembly: System.Reflection.AssemblyProductAttribute("semana15")]
+[assembly: System.Reflection.AssemblyTitleAttribute("semana15")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
